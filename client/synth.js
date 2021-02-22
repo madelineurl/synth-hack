ad0af16881d94f135e7bade6db54adf9573a1cfb
Maddie Higgins <maddiehiggins9@gmail.com>
2021-02-22 12:36:05 -0600

    add wave type selection

--- client/synth.js
+++ client/synth.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Octave from "./octave";
 import * as Tone from 'tone';
 
+
 const Synth = () => {
-  const synth = new Tone.Synth().toDestination();
+  const [waveType, setWaveType] = useState('sine');
+
+  const selectWaveType = (evt) => {
+    setWaveType(evt.target.value);
+  };
+
+  const synth = new Tone.PolySynth(Tone.Synth, {
+    oscillator: {
+      type : waveType
+    }
+  }).toDestination();
 
   return (
-    <ul className='keyboard'>
-      <Octave octave='3' synth={synth} />
-      <Octave octave='4' synth={synth} />
-      <Octave octave='5' synth={synth}/>
-    </ul>
+    <div className='synth'>
+      <label htmlFor='select-wave'>WAVE</label>
+      <select id='select-wave' onChange={selectWaveType}>
+          <option>sine</option>
+          <option>square</option>
+          <option>triangle</option>
+          <option>sawtooth</option>
+      </select>
+
+      <ul className='keyboard'>
+        <Octave octave='3' synth={synth} />
+        <Octave octave='4' synth={synth} />
+        <Octave octave='5' synth={synth}/>
+      </ul>
+    </div>
   );
 };
 
