68c874309636aad297fa3502d033a0ee3ab82417
Maddie Higgins <maddiehiggins9@gmail.com>
2021-02-24 13:26:27 -0600

    add main volume control

--- client/synth.js
+++ client/synth.js
@@ -7,6 +7,11 @@ import * as Tone from 'tone';
 const Synth = () => {
   const [waveType, setWaveType] = useState('sine');
   const [lowestOctave, setLowestOctave] = useState(3);
+  const [volume, setVolume] = useState(-6);
+
+  const setMainVol = (evt) => {
+    setVolume(evt.target.value);
+  };
 
   const selectWaveType = (evt) => {
     setWaveType(evt.target.value);
@@ -16,11 +21,15 @@ const Synth = () => {
     setLowestOctave(parseInt(evt.target.value));
   };
 
+  const mainVol = new Tone.Volume(volume);
+
   const synth = new Tone.PolySynth(Tone.Synth, {
     oscillator: {
       type: waveType
     }
-  }).toDestination();
+  }).chain(mainVol, Tone.Destination);
+
+  // console.log(synth.activeVoices);
 
   return (
     <>
@@ -29,29 +38,38 @@ const Synth = () => {
       <li><button>how-to</button></li>
     </ul>
     <div className='synth'>
-      <label htmlFor='select-wave'>WAVE</label>
-      <select
-        id='select-wave'
-        onChange={selectWaveType}
-        value={waveType}
-      >
-          <option>sine</option>
-          <option>square</option>
-          <option>triangle</option>
-          <option>sawtooth</option>
-      </select>
-      <label htmlFor='select-octave'>OCTAVE</label>
-      <select
-        id='select-octave'
-        onChange={selectLowestOctave}
-        value={lowestOctave}
-      >
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
+      <div id='main'>
+        <label htmlFor='main-vol'>vol</label>
+        <input id='main-vol'type='range' min={-16} max={4} value={volume} onChange={setMainVol} />
+        <div>tempo</div>
+      </div>
+      <div id='vco-controls'>
+        <div id='vco1'>
+          <label htmlFor='select-wave'>WAVE</label>
+          <select
+            id='select-wave'
+            onChange={selectWaveType}
+            value={waveType}
+          >
+              <option>sine</option>
+              <option>square</option>
+              <option>triangle</option>
+              <option>sawtooth</option>
+          </select>
+          <label htmlFor='select-octave'>OCTAVE</label>
+          <select
+            id='select-octave'
+            onChange={selectLowestOctave}
+            value={lowestOctave}
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          </div>
+      </div>
       <ul className='keyboard'>
         <Octave octave={lowestOctave} synth={synth} />
         <Octave octave={lowestOctave + 1} synth={synth} />
