0665fc0f8e5dfdbc5a1ae0a93a3a1ac95b52c79b
Maddie Higgins <maddiehiggins9@gmail.com>
2021-02-24 15:18:19 -0600

    add remaining synth structure

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Synth Time
-This is a basic web-based synthesizer built using Tone.js and React.
+# Synth Time!
+This is a basic web-based synthesizer inspired by the Korg Minilogue. It's a highly oversimplified version in its current state, but I plan to continue building on it until I have a fairly convincing Minilogue clone. Built using Tone.js and React.
--- client/synth.js
+++ client/synth.js
@@ -10,16 +10,35 @@ const Synth = () => {
   const [volume, setVolume] = useState(-6);
   const [pitch, setPitch] = useState(0);
   const [bpm, setBpm] = useState(120);
+  const [voiceMode, setVoiceMode] = useState('mono');
 
   const mainVol = new Tone.Volume(volume);
   Tone.Transport.bpm.value = bpm;
   // add metronome to test
 
-  const synth = new Tone.Synth({
-    oscillator: {
-      type: waveType
-    }
-  }).chain(mainVol, Tone.Destination);
+  let synth;
+  switch(voiceMode) {
+    case 'mono':
+      synth = new Tone.MonoSynth({
+        oscillator: {
+          type: waveType
+        }
+      }).chain(mainVol, Tone.Destination);
+      break;
+    case 'poly':
+      synth = new Tone.PolySynth(Tone.Synth, {
+        oscillator: {
+          type: waveType
+        }
+      }).chain(mainVol, Tone.Destination);
+      break;
+    default:
+      synth = new Tone.MonoSynth({
+        oscillator: {
+          type: waveType
+        }
+      }).chain(mainVol, Tone.Destination);
+  }
 
   const adjustMainVol = (evt) => {
     setVolume(evt.target.value);
@@ -43,6 +62,10 @@ const Synth = () => {
     setLowestOctave(parseInt(evt.target.value));
   };
 
+  function selectInstrument(evt) {
+    setVoiceMode(evt.target.value);
+  }
+
   return (
     <>
     <ul id='navbar'>
@@ -50,47 +73,104 @@ const Synth = () => {
       <li><button>how-to</button></li>
     </ul>
     <div className='synth'>
-      <div id='main'>
-        <label htmlFor='main-vol'>vol</label>
-        <input id='main-vol'type='range' min={-16} max={4} value={volume} onChange={adjustMainVol} />
-        <label htmlFor='main-tempo'>tempo</label>
-        <input id='main-tempo'type='range' min={80} max={200} value={bpm} onChange={adjustTempo} />
-      </div>
-      <div id='controls'>
-          <label htmlFor='select-wave'>WAVE</label>
-          <select
-            id='select-wave'
-            onChange={selectWaveType}
-            value={waveType}
-          >
-              <option>sine</option>
-              <option>square</option>
-              <option>triangle</option>
-              <option>sawtooth</option>
-          </select>
-          <label htmlFor='select-octave'>OCTAVE</label>
-          <select
-            id='select-octave'
-            onChange={selectLowestOctave}
-            value={lowestOctave}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-          <label htmlFor='select-octave'>PITCH ({pitch}c)</label>
+      <div className='controls flex'>
+        <div id='main' className='section flex'>
+          <label htmlFor='main-vol'>vol</label>
           <input
-            id='pitch'
+            id='main-vol'
             type='range'
-            min={-1200}
-            max={1200}
-            onChange={adjustPitch}
-            value={pitch}
+            min={-16}
+            max={4}
+            value={volume}
+            onChange={adjustMainVol}
           />
+          <label htmlFor='main-tempo'>tempo</label>
+          <input
+            id='main-tempo'
+            type='range'
+            min={80}
+            max={200}
+            value={bpm}
+            onChange={adjustTempo}
+          />
+          <label htmlFor='select-wave'>VOICE MODE</label>
+              <select
+                id='select-wave'
+                onChange={selectInstrument}
+                value={voiceMode}
+              >
+                <option>mono</option>
+                <option>poly</option>
+            </select>
+        </div>
+        <div id='main' className='section flex'>
+            <label htmlFor='select-wave'>WAVE</label>
+            <select
+              id='select-wave'
+              onChange={selectWaveType}
+              value={waveType}
+            >
+                <option>sine</option>
+                <option>square</option>
+                <option>triangle</option>
+                <option>sawtooth</option>
+            </select>
+            <label htmlFor='select-octave'>OCTAVE</label>
+            <select
+              id='select-octave'
+              onChange={selectLowestOctave}
+              value={lowestOctave}
+            >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            <label htmlFor='select-octave'>PITCH ({pitch}c)</label>
+            <input
+              id='pitch'
+              type='range'
+              min={-1200}
+              max={1200}
+              onChange={adjustPitch}
+              value={pitch}
+            />
+        </div>
+        <div id='filter' className='section flex'>
+          <label htmlFor='filter-adjust'>cutoff</label>
+            <input
+              id='filter-adjust'
+              type='range'
+              min={80}
+              max={200}
+              // onChange={adjustFilter}
+            />
+        </div>
+        <div id='envelope' className='section flex'>
+          <label htmlFor='attack'>attack</label>
+            <input
+              id='attack'
+              type='range'
+            />
+            <label htmlFor='decay'>decay</label>
+            <input
+              id='decay'
+              type='range'
+            />
+            <label htmlFor='attack'>sustain</label>
+            <input
+              id='sustain'
+              type='range'
+            />
+            <label htmlFor='attack'>release</label>
+            <input
+              id='release'
+              type='range'
+            />
+        </div>
       </div>
-      <ul className='keyboard'>
+      <ul className='keyboard flex'>
         <Octave octave={lowestOctave} synth={synth} />
         <Octave octave={lowestOctave + 1} synth={synth} />
         <Octave octave={lowestOctave + 2} synth={synth}/>
